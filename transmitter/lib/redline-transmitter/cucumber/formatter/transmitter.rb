35d44cdb4d7eeb13f300a8dfa15a89ce4008ffc8
Bramha Ghosh <bramhaghosh@gmail.com>
2012-04-27 14:53:22 -0400

    created a formatter

---
+++ transmitter/lib/redline-transmitter/cucumber/formatter/transmitter.rb
@@ -0,0 +1,14 @@
+require 'cucumber'
+require 'cucumber/formatter'
+
+module Redline
+  module Cucumber
+    module Formatter
+      class Transmitter
+        def initialize(step_mother, io, options)
+
+        end
+      end
+    end
+  end
+end
