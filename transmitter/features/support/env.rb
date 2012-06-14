58fe75596597305e20c46a14d55690d97f1e22d8
Bramha Ghosh <bramhaghosh@gmail.com>
2012-06-14 12:32:15 -0400

    cleanup env.rb

--- transmitter/features/support/env.rb
+++ transmitter/features/support/env.rb
@@ -1,14 +1,12 @@
 require 'pry'
+require 'aruba/api'
+require 'aruba/cucumber'
 
 $: << File.dirname(__FILE__) + "/../../lib"
 
 # PROJECT_ROOT = File.join(File.dirname(__FILE__),'..','..')
 # ENV['PATH'] = "#{File.join(PROJECT_ROOT,'lib')}#{File::PATH_SEPARATOR}#{ENV['PATH']}"
 
-puts "Path is #{ENV['PATH']}"
-
-require 'aruba/api'
-require 'aruba/cucumber'
 
 World(Aruba::Api)
 
