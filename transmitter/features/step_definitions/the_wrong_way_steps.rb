e936ca517ec5471462f0987f1577f5217f49ab9a
Bramha Ghosh <bramhaghosh@gmail.com>
2012-06-15 12:58:53 -0400

    remove unneeded require

--- transmitter/features/step_definitions/the_wrong_way_steps.rb
+++ transmitter/features/step_definitions/the_wrong_way_steps.rb
@@ -1,9 +1,4 @@
-require 'aruba/cucumber'
 Given /^I have the formatter$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-When /^it runs$/ do
   pending # express the regexp above with the code you wish you had
 end
 
