ecb8ce37dbf9b1e201fe064e13ad373b86ddcb6b
Bramha Ghosh <bramhaghosh@gmail.com>
2012-06-15 16:00:09 -0400

    remove unused steps

--- transmitter/features/step_definitions/the_wrong_way_steps.rb
+++ transmitter/features/step_definitions/the_wrong_way_steps.rb
@@ -1,12 +1,3 @@
-Given /^I have the formatter$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^it calls the service$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-
 Given /^a valid feature with step definitions$/ do
   step_file = 'dummy_steps.rb'
   path = File.expand_path("#{FIXTURE_ROOT}/#{step_file}")
@@ -16,10 +7,6 @@ Given /^a valid feature with step definitions$/ do
   path = File.expand_path("#{FIXTURE_ROOT}/#{feature_file}")
   feature = File.read(path)
   write_file("features/#{feature_file}", feature)
-end
-
-
-Then /^I should not get any error messages$/ do
 end
 
 Then /I require the Redline::Transmitter formatter$/ do
--- transmitter/features/transmitter_formatter.feature
+++ transmitter/features/transmitter_formatter.feature
@@ -4,8 +4,7 @@ Feature: Use the transmitter formatter
 
   Scenario: Requiring the formatter
     And I require the Redline::Transmitter formatter
-    When I successfully run `cucumber -f Redline::Transmitter`
-    Then I should not get any error messages
+    Then I successfully run `cucumber -f Redline::Transmitter`
 
   Scenario: Failing to require the formatter
     When I run `cucumber -f Redline::Transmitter`
